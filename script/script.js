2d6e0ba0390e29a7a605eee73e46b62d357e5a7f
jonas.moeneclaey <jonas.moeneclaey@student.staam.be>
2025-05-12 11:22:07 +0200

    1ste commit

--- script/script.js
+++ script/script.js
@@ -1,6 +1,13 @@
 const form = document.getElementById("todo-form");
 const input = document.getElementById("todo-input");
 const list = document.getElementById("todo-list");
+const winkelsDiv = document.createElement("div")
+const winkels = ["colruyt", "aldi", "lidl", "delhaize"]
+const voorafIngesteldeTaken = ["appels (colruyt)", "yogurt (aldi)", "oplaadkabel (aldi)","fruitsap (delhaize)"]
+const winkelSelect = document.createElement("select")
+list.appendChild(winkelsDiv)
+winkelsDiv.className = "winkelsDiv"
+winkelsDiv.appendChild(winkelSelect)
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -15,3 +22,16 @@ form.addEventListener("submit", function (e) {
     input.value = "";
   }
 });
+
+voorafIngesteldeTaken.forEach((taak) => {
+  const li = document.createElement("li")
+  li.textContent = taak
+  li.addEventListener("click", function () {
+    li.classList.toggle("completed");})
+  list.appendChild(li)
+})
+winkels.forEach((winkel) => {
+  const optie = document.createElement("option")
+  optie.textContent = winkel
+  winkelSelect.appendChild(optie)
+})
